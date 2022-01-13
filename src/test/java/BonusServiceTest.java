47700e7fce7bb14099a7fce9b039d9da8055f6c9
Alexander Syayalev <alexxs@inbox.ru>
2022-01-13 23:01:21 +0300

    before download

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,6 +33,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
